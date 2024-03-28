73000ec572aef49cadac1ccb7b854e721c84098c
grigory <gtukmachev@gmail.com>
2024-03-28 16:20:49 +0000

    logo

--- src/main/kotlin/tga/backup/logo/logo.kt
+++ src/main/kotlin/tga/backup/logo/logo.kt
@@ -3,16 +3,16 @@ package tga.backup.logo
 fun printLogo() {
     println(
         """
-        |   +--------- Backup utility --------------------------------------------+
-        |   |  Syncs one folder to another using minimum file-system operations.  |
-        |   |  Skips the same files.                                              |
-        |   |  Supports local file-system and clouds: Yandex Disk, Google drive   |
-        |   |                                                                     |
-        |   |  how to run:                                                        |
-        |   |                                                                     |
-        |   |  $> backup -s <source> -d <destination> [--dry-run]                 |
-        |   |                                                                     |
-        |   +---------------------------------------------------------------------+
+        |   +--------- Backup utility ------------------------------------------------------------------+
+        |   |  Syncs one folder to another using minimum file-system operations.                        |
+        |   |  Skips the same files.                                                                    |
+        |   |  Supports local file-system and clouds: Yandex Disk, Google drive                         |
+        |   |                                                                                           |
+        |   |  how to run:                                                                              |
+        |   |                                                                                           |
+        |   |  $> backup -s <source> -d <destination> [--dry-run] [--show-src] [--show-dst] [--verbose] |
+        |   |                                                                                           |
+        |   +-------------------------------------------------------------------------------------------+
         """.trimMargin()
     )
 
