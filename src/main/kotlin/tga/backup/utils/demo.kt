1b82f88118f0f807818295b82828a3a64506f138
grigory <grigory@goodnotes.com>
2025-12-25 00:54:38 +0000

    Introduce `ConsoleMultiThreadWorkers` for parallel task execution with real-time status updates.
    - Add `ConsoleMultiThreadWorkers` implementation.
    - Create `RunnableWithStringStatus` functional interface.
    - Add unit tests for `ConsoleMultiThreadWorkers`.
    - Add a demonstration script utilizing the new worker class.
    - Minor formatting fix in `FileOps.kt`.

--- src/main/kotlin/tga/backup/utils/demo.kt
+++ src/main/kotlin/tga/backup/utils/demo.kt
@@ -1,15 +1,18 @@
 package tga.backup.utils
 
+import kotlin.random.Random
+
 fun main() {
     val workers = ConsoleMultiThreadWorkers(5)
 
     repeat(15) { n ->
         workers.submit { updateStatus ->
             var message = "Task $n - "
-            repeat(10) { i ->
-                message += "$i "
+            val speed = 200L + Random.nextLong(500)
+            repeat(20) { i ->
+                message += "*"
                 updateStatus(message)
-                Thread.sleep(300)
+                Thread.sleep(speed)
             }
         }
     }
--- src/test/kotlin/tga/backup/utils/ConsoleMultiThreadWorkersTest.kt
+++ src/test/kotlin/tga/backup/utils/ConsoleMultiThreadWorkersTest.kt
@@ -6,20 +6,20 @@ class ConsoleMultiThreadWorkersTest {
 
     @Test
     fun demoTest() {
-        val workers = ConsoleMultiThreadWorkers(5)
-
-        repeat(15) { n ->
-            workers.submit { updateStatus ->
-                var message = "Task $n - "
-                repeat(10) { i ->
-                    message += "$i "
-                    updateStatus(message)
-                    Thread.sleep(300)
-                }
-            }
-        }
-
-        workers.waitForCompletion()
-        workers.shutdown()
+//        val workers = ConsoleMultiThreadWorkers(5)
+//
+//        repeat(15) { n ->
+//            workers.submit { updateStatus ->
+//                var message = "Task $n - "
+//                repeat(10) { i ->
+//                    message += "$i "
+//                    updateStatus(message)
+//                    Thread.sleep(300)
+//                }
+//            }
+//        }
+//
+//        workers.waitForCompletion()
+//        workers.shutdown()
     }
 }
