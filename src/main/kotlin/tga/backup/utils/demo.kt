b783a3b529e238df0b3cf952f64f1b99e2d2e724
grigory <grigory@goodnotes.com>
2025-12-25 01:01:53 +0000

    Add color-coded status updates to task progress in demo and tests.

--- src/main/kotlin/tga/backup/utils/demo.kt
+++ src/main/kotlin/tga/backup/utils/demo.kt
@@ -10,8 +10,15 @@ fun main() {
             var message = "Task $n - "
             val speed = 200L + Random.nextLong(500)
             repeat(20) { i ->
+                val ratio = i / 20.0
+                val colorCode = when {
+                    ratio < 0.25 -> "\u001b[90m" // Dark Gray
+                    ratio < 0.50 -> "\u001b[34m" // Blue
+                    ratio < 0.75 -> "\u001b[94m" // Light Blue
+                    else -> "\u001b[96m"        // Light Cyan (very light blueish)
+                }
                 message += "*"
-                updateStatus(message)
+                updateStatus("$colorCode$message\u001b[0m")
                 Thread.sleep(speed)
             }
         }
--- src/test/kotlin/tga/backup/utils/ConsoleMultiThreadWorkersTest.kt
+++ src/test/kotlin/tga/backup/utils/ConsoleMultiThreadWorkersTest.kt
@@ -11,10 +11,17 @@ class ConsoleMultiThreadWorkersTest {
 //        repeat(15) { n ->
 //            workers.submit { updateStatus ->
 //                var message = "Task $n - "
-//                repeat(10) { i ->
+//                repeat(20) { i ->
+//                    val ratio = i / 20.0
+//                    val colorCode = when {
+//                        ratio < 0.25 -> "\u001b[90m" // Dark Gray
+//                        ratio < 0.50 -> "\u001b[34m" // Blue
+//                        ratio < 0.75 -> "\u001b[94m" // Light Blue
+//                        else -> "\u001b[96m"        // Light Cyan
+//                    }
 //                    message += "$i "
-//                    updateStatus(message)
-//                    Thread.sleep(300)
+//                    updateStatus("$colorCode$message\u001b[0m")
+//                    Thread.sleep(100)
 //                }
 //            }
 //        }
